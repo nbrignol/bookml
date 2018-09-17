6c54bca1b8ce62b9875ad82363331970607093cf
Nicolas Brignol <tools@octet-studio.com>
2018-09-17 14:52:51 +0200

    Add sample

--- index.js
+++ index.js
@@ -1,3 +1,49 @@
-exports.bookml = function() {
-  console.log("This is a message from the demo package");
+var fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+exports.BookML = function() {
+
+  this.toc = {};
+
+  this.hello = function(){
+    return "Hello, BookML";
+  }
+
+  this.read = function(filename){
+    return fs.readFileSync(filename).toString();
+  }
+
+  this.isHeading = function(tag) {
+    const headings = ['H1', 'H2', 'H3'];
+    return headings.indexOf(tag) >= 0 ? true : false;
+  }
+
+  this.process = function(){
+    var source = new JSDOM(this.read("sample/sample.html"));
+    var output = new JSDOM();
+
+    var style = output.window.document.createElement("style");
+    style.textContent = this.read("sample/sample.css");
+    output.window.document.head.appendChild(style);
+
+    source.window.document.body.childNodes.forEach(function(element) {
+
+        if (this.isHeading(element.tagName)) {
+             var span = output.window.document.createElement("span");
+             span.innerHTML = element.innerHTML;
+
+             element.innerHTML = "";
+             element.appendChild(span);
+
+        }
+
+        output.window.document.body.appendChild(element);
+    }, this);
+
+
+
+    return output.serialize();
+
+  }
 }
---
+++ test.js
@@ -0,0 +1,5 @@
+const BookML = require("./index");
+const bookml = new BookML.BookML();
+
+//console.log(bookml.hello());
+console.log(bookml.process());
